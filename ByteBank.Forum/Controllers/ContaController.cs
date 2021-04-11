242c331e244eb2ee59396678846b87d2e09b6b85
William Fernandes Magalhaes <williamfmagalhaes@hotmail.com>
2021-04-11 00:08:15 -0300

    criando a view

---
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Forum.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
